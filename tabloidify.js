d531028cea2e5ec734b0ba3b975edacef4e44435
Rui Figueira <rui.figueira@gmail.com>
2017-02-22 11:12:48 +0000

    Added more prepositions

--- tabloidify.js
+++ tabloidify.js
@@ -14,7 +14,7 @@ var headline = headlines.eq(Math.floor(Math.random() * headlines.waitForExistenc
 var headlineText = String(headline.waitForExistence().text().trim());
 
 var tabloidified = headlineText
-  .replace(/^(.*) (por|para|de|do|da|no|na|em|à|ao|a|antes|depois|que|quando|onde|como|só|ainda|se) (.*)$/ig, "$1... $2 $3!");
+  .replace(/^(.*) (por|para|de|do|da|no|na|em|à|ao|a|antes|depois|que|quando|onde|como|só|ainda|se|este|esta|estes|aquele|aquela|aquilo|neste|nesta|naquilo|naquele|naquela) (.*)$/ig, "$1... $2 $3!");
 
 var url = "http://www.dn.pt" + headline.parent("a").attr("href");
 
